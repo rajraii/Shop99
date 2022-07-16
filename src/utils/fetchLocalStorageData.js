49e0d870348c4726efde5c52b620d16d985e7141
Raj Rai <rrai1590@gmail.com>
2022-07-17 00:41:23 +0530

    make it better

--- src/utils/fetchLocalStorageData.js
+++ src/utils/fetchLocalStorageData.js
@@ -6,8 +6,12 @@ export const fetchUser = () => {
 };
 
 export const fetchCart = () => {
+    
+    
     const cartInfo = localStorage.getItem('cartItems') !== "undefined" 
         ? JSON.parse(localStorage.getItem('cartItems'))
         : localStorage.clear()
     return cartInfo ? cartInfo : [];    
 };
+
+
